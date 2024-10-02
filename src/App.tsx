24daa3699f5b2a285b8bc65c02e6599fa99ce246
Jeremy Hansen <jhansen1344@gmail.com>
2024-10-02 17:10:36 -0400

    adds styling and header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StringToListContainer from './components/StringToListContainer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <StringToListContainer inputString={"(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)"} />
+    <>
+      <Header />
+      <StringToListContainer inputString={"(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)"} />
+    </>
   );
 }
 
